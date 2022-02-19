13288ad7a2567782120328f31c084ed14c7b3c63
AcesofBases <83755522+AcesofBases@users.noreply.github.com>
2022-02-19 15:08:08 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+peter_pan="";
+harry_potter="";
+function Preload(){
+peter_pan=loadSound("music.mp3");
+harry_potter=loadSound("music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+    
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
